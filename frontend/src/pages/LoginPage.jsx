6a26d4f51b92bb5d3cda9957e995e839a8eb5c86
Renelle <rq@HYMacBookPro.local>
2024-12-10 11:55:03 -0800

    add global context: AuthContext jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,14 +1,17 @@
-/**
- * User profile page
- */
 import '../styles/LoginPage.css';
-
 import React from 'react';
+import LoginForm from '../components/LoginForm';
+import { AuthContext } from '../context/AuthContext';
 
 const LoginPage = () => {
+  const handleLoginSuccess = (token) => {
+    console.log('Token received:', token);
+    // Set token in global state, cookies, or localStorage
+  };
+
   return (
     <div>
-      <p>content</p>
+      <LoginForm onLoginSuccess={handleLoginSuccess} />
     </div>
   );
 };
