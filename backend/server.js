9f7c6d146c2cb261d4afe7083e8100ae4e646b9a
Renelle <rq@HYMacBookPro.local>
2024-12-11 01:38:46 -0800

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,6 @@ app.get('*', function (req, res) {
 });
 
 // Start the server
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 8000, function() {
     console.log("Starting server now...")
 })
