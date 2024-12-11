821c0bdb4a72aab82949309efb99f62d4a886a2a
Lipeipei Sun <137748132+sunlipeipei@users.noreply.github.com>
2024-12-11 09:01:46 -0800

    updated UserProfile

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,3 +1,15 @@
 /**
  * Display user profile details
  */
+import '../styles/PostList.css';
+import axios from "axios";
+import { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+
+export default function UserProfile(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
