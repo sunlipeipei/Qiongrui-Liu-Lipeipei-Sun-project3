2ccd5a57ef68138b7f0b260e27f30826cdc9ea5b
Renelle <rq@HYMacBookPro.local>
2024-12-12 17:17:04 -0800

    immediately be logged in after sign up

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -45,7 +45,7 @@ const SignupPage = () => {
       setSuccessMessage('Signup and login successful! Redirecting...');
       setTimeout(() => {
         window.location.href = '/';
-      }, 1000);
+      }, 100);
 
     } catch (error) {
       console.error(error.response?.data || error.message);
